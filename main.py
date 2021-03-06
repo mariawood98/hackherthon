1ff98e687b12e7d566f7b7c852f97db0b9a29c3d
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-03-06 18:59:00 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -6,15 +6,30 @@ r1 = sr.Recognizer()
 r2 = sr.Recognizer()
 r3 = sr.Recognizer()
 now = datetime.datetime.now()
-print("type in contact number: ")
-contact_number = input()
+
+print("type in contact number 1: ")
+contact_number1 = input()
+print("type in contact number 2: ")
+contact_number2 = input()
+
+print("type in awaken word:")
+awaken_word = input()
+
+print("contact 1 trigger word:")
+trigger1 = input()
+print("contact 2 trigger word:")
+trigger2 = input()
+
 with sr.Microphone() as source:
     print("speak now")
     audio = r3.listen(source)
 
-if 'hello' in r2.recognize_google(audio):
+if awaken_word in r2.recognize_google(audio):
     r2 = sr.Recognizer()
-    if 'maddie' in r2.recognize_google(audio):
-        pywhatkit.sendwhatmsg("contact_number", "hi", now.hour, now.minute+2)
-    elif 'cathy' in r2.recognize_google(audio):
-        pywhatkit.sendwhatmsg("+contact_number", "hi", now.hour, now.minute+2)
+    print("speak now")
+    with sr.Microphone() as source:        
+        audio = r2.listen(source)   
+        if trigger1 in r2.recognize_google(audio):
+            pywhatkit.sendwhatmsg(contact_number1, "hi", now.hour, now.minute+2)
+        elif trigger2 in r2.recognize_google(audio):
+            pywhatkit.sendwhatmsg(contact_number2, "hi", now.hour, now.minute+2)
